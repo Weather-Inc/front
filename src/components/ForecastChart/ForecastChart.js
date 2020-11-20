a643e0b74ca3530c5a1ff9867ccfd3acf053c23a
Stéphane Noutsa <stephanenoutsak@gmail.com>
2020-11-20 15:02:32 +0100

    Renders forecast data with FusionCharts

--- src/components/ForecastChart/ForecastChart.js
+++ src/components/ForecastChart/ForecastChart.js
@@ -1,8 +1,51 @@
 import { Card, CardContent, Chip, CircularProgress, Container, Grid } from '@material-ui/core'
+import FusionCharts from 'fusioncharts'
+import Charts from 'fusioncharts/fusioncharts.charts'
+import ReactFC from 'react-fusioncharts'
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion'
+import moment from 'moment'
+
 import forecastChartStyles from 'assets/styles/forecast-chart/forecastChart'
+
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme)
+
+const formattedTime = (date) => moment(date).format('HH:mm')
+
+const formattedDate = (date) => moment(date).format('dddd, MMMM Do YYYY')
 
 const forecastChart = props => {
   const classes = forecastChartStyles()
+  const forecasts = props.forecasts
+
+  const chartData = []
+  let date = formattedDate()
+
+  if (forecasts.length) {
+    const forecast = forecasts[0]
+    date = formattedDate(forecast.date)
+
+    forecast.forecast.forEach(f => {
+      chartData.push({
+        label: formattedTime(f.startTime),
+        value: f.temperatures.value.toFixed(1)
+      })
+    })
+  }
+
+  const chartConfigs = {
+    type: 'column2d',
+    dataFormat: 'json',
+    dataSource: {
+      chart: {
+        caption: `Weather Forecast for ${date}`,
+        xAxisName: 'Time of Day',
+        yAxisName: 'Temperature (°C)',
+        numberSuffix: '',
+        theme: 'fusion'
+      },
+      data: chartData
+    }
+  }
 
   const handleTodayClicked = () => {
     props.onTodayClicked()
@@ -40,7 +83,10 @@ const forecastChart = props => {
 
       <Card className={classes.card}>
         <CardContent className={classes.cardContent}>
-          {props.loading && <CircularProgress />}
+          {props.loading
+            ? <CircularProgress />
+            : <ReactFC {...chartConfigs} />
+          }
         </CardContent>
       </Card>
     </Container>
